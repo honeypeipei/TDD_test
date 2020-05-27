9eef65ea12c6f6203b2e92312e646ccd8cced9b7
honeypeipei <478313194@qq.com>
2020-05-27 16:24:17 +0800

    217页

--- superlists/lists/forms.py
+++ superlists/lists/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from lists.models import Item
+from django.core.exceptions import ValidationError
 
 EMPTY_ITEM_ERROR = "You can't have an empty list item"
+DUPLICATE_ITEM_ERROR = "You've already got this in your list"
 
 #django提供了一个特殊的类ModelForm，用来生成模型的表单
 class ItemForm(forms.models.ModelForm):
@@ -25,10 +27,15 @@ class ItemForm(forms.models.ModelForm):
 		self.instance.list = for_list
 		return super().save()
 
-	# item_text = forms.CharField(
-	# 	widget = forms.fields.TextInput(attrs = {
-	# 		'placeholder' : 'Enter a to-do item',
-	# 		'class' : 'form-control input-lg',
-	# 		}),
-	# )
+class ExistingListItemForm(ItemForm):
+	def __init__(self, for_list, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		self.instance.list = for_list
+
+	def validate_unique(self):
+		try:
+			self.instance.validate_unique()
+		except ValidationError as e:
+			e.error_dict = { 'text' : [DUPLICATE_ITEM_ERROR]}
+			self._update_errors(e)
 
