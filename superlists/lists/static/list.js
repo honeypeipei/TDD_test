21604c60a4cbe9ff352221c0d562bba88db23571
honeypeipei <478313194@qq.com>
2020-05-29 17:08:54 +0800

    Chapter16

--- superlists/lists/static/list.js
+++ superlists/lists/static/list.js
@@ -1,9 +1,7 @@
 
-var initialize = function(){
-	// console.log('initialize called');
+window.Superlists = {};
+window.Superlists.initialize = function(){
 	$('input[name = "text"]').on('keypress', function(){
-		console.log('in keypress handler');
 		$('.has-error').hide();
 	});
 };
-console.log('list.js loaded')
